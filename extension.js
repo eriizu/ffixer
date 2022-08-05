306799285edd567556ac80ea32135098335dbc56
Élise <elise@eiss.fr>
2022-08-05 23:55:15 +0200

    now compatible with multiscreen worspaces, always on top windows and all worspaces windows
    it also fixes an issue where focusing the right window would be overidden by default behaviour. it does so by waiting 1 ms, which is technically not a fix but just a workarround (basically this whole extension is a work arround for bad design)

--- extension.js
+++ extension.js
@@ -1,25 +1,30 @@
-const { Meta } = imports.gi;
+const { GObject, Meta } = imports.gi;
 
 class Extension {
     _onActiveWorkspaceChanged() {
-        if (!Meta.prefs_get_workspaces_only_on_primary())
-            return;
-
         const activeWs = global.workspace_manager.get_active_workspace();
-        let windows = activeWs.list_windows()
-              .filter(w => w.is_on_primary_monitor());
+        let windows = activeWs.list_windows();
+        if (Meta.prefs_get_workspaces_only_on_primary()) {
+            windows = windows.filter(w => w.is_on_primary_monitor());
+        } else {
+              windows = windows.filter(w => !(w.is_on_all_workspaces() || w.is_above()));
+        }
         const newFocus = windows.reduce((prev, current) => {
             if (current.get_user_time() > prev.get_user_time()) {
                 return current;
             }
             return prev;
         });
-        newFocus?.activate(global.get_current_time());
+        if (newFocus)
+            console.log(newFocus.get_title(), newFocus.is_on_all_workspaces());
+        else
+            console.log("no window to focus");
+        setTimeout(() => newFocus?.focus(global.get_current_time()), 1);
     }
 
     enable() {
         global.workspace_manager.connectObject(
-            'active-workspace-changed', () => this._onActiveWorkspaceChanged(),
+            'active-workspace-changed', () => this._onActiveWorkspaceChanged(), GObject.ConnectFlags.AFTER,
             this);
     }
 
